359e195e6c16c4e109d5af9b0edcbc42625ffd80
Orquidea Marques <orquidea_238@hotmail.fr>
2019-05-07 14:54:01 +0200

    variables js

---
+++ ex01/index.js
@@ -0,0 +1,12 @@
+let numeros = 12;
+
+let chaineCaracteres = "Hello World";
+
+let cVrai = true;
+
+let objet = {};
+
+console.log(typeof 12);
+console.log(typeof "Hello World");
+console.log(typeof true);
+console.log(typeof {});
